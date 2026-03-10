5c994d4917fc3a2588447334bc1129c2af3b70a0
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2026-03-10 11:49:07 +0000

    Remove old context domain

--- app/context/dependencies.py
+++
@@ -1,5 +0,0 @@
-from app.context.service import ContextService
-
-
-def get_context_service() -> ContextService:
-    return ContextService()
--- app/context/models.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class UploadInitiation:
-    upload_id: str
